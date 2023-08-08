4ea2fad7cf8cd9e528e42b06d5975d1806e5b079
Evandro Viegas <evandrooviegas@gmail.com>
2023-08-08 16:23:59 +0100

    fixed bug

--- app/(pages)/About/index.tsx
+++ app/(pages)/About/index.tsx
@@ -12,7 +12,7 @@ export default function About() {
       id="about"
       className="grid grid-cols-2 items-center md:gap-4 gap-12  py-6"
     >
-      <div className="space-y-2 w-full">
+      <div className="space-y-2 w-full md:col-span-1 col-span-2">
         <h4 className="text-2xl font-bold text-primary">{title}</h4>
         <p className="border-l-4 border-primary p-4 text-neutral-800 shadow ">
           {text}
