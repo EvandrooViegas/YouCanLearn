4e5581484d883e4c3917d7c68b1ddec5b92a15fd
Evandro Viegas <evandrooviegas@gmail.com>
2024-01-16 21:15:16 +0000

    removed whatsapp link btn

--- components/WhatsappButton/index.tsx
+++ components/WhatsappButton/index.tsx
@@ -3,7 +3,7 @@ import { BsWhatsapp } from "react-icons/bs"
 export default function WhatsappButton() {
   return (
     <button className='bg-green-400 fixed bottom-10 right-10 p-4 rounded-full'>
-        <a href="https://chat.whatsapp.com/EXOUVkzOGcv3xPeYtHUlAQ">
+        <a href="">
             <BsWhatsapp className='text-white' size="35px" />
         </a>
     </button>
